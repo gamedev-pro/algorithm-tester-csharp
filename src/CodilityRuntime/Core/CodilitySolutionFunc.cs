6bea6d96d04b1eb24a6537734b481680a068ae6e
Gabriel Dechichi <gabrieldechichi@gmail.com>
2020-10-04 19:45:24 -0600

    Added support for solutions with 4 and 5 params

--- src/CodilityRuntime/Core/CodilitySolutionFunc.cs
+++ src/CodilityRuntime/Core/CodilitySolutionFunc.cs
@@ -43,4 +43,35 @@ namespace CodilityRuntime.Core
             };
         }
     }
+
+    class CodilitySolutionFunc<T1, T2, T3, T4, TRet>
+    {
+        public static Func<IEnumerable<object>, IEnumerable<object>> Get(Func<T1, T2, T3, T4, TRet> func)
+        {
+            return (input) =>
+            {
+                T1 typedInput1 = JsonConvert.DeserializeObject<T1>(JsonConvert.SerializeObject(input.ElementAt(0)));
+                T2 typedInput2 = JsonConvert.DeserializeObject<T2>(JsonConvert.SerializeObject(input.ElementAt(1)));
+                T3 typedInput3 = JsonConvert.DeserializeObject<T3>(JsonConvert.SerializeObject(input.ElementAt(2)));
+                T4 typedInput4 = JsonConvert.DeserializeObject<T4>(JsonConvert.SerializeObject(input.ElementAt(3)));
+                return new List<object>() { func(typedInput1, typedInput2, typedInput3, typedInput4) };
+            };
+        }
+    }
+
+    class CodilitySolutionFunc<T1, T2, T3, T4, T5, TRet>
+    {
+        public static Func<IEnumerable<object>, IEnumerable<object>> Get(Func<T1, T2, T3, T4, T5, TRet> func)
+        {
+            return (input) =>
+            {
+                T1 typedInput1 = JsonConvert.DeserializeObject<T1>(JsonConvert.SerializeObject(input.ElementAt(0)));
+                T2 typedInput2 = JsonConvert.DeserializeObject<T2>(JsonConvert.SerializeObject(input.ElementAt(1)));
+                T3 typedInput3 = JsonConvert.DeserializeObject<T3>(JsonConvert.SerializeObject(input.ElementAt(2)));
+                T4 typedInput4 = JsonConvert.DeserializeObject<T4>(JsonConvert.SerializeObject(input.ElementAt(3)));
+                T5 typedInput5 = JsonConvert.DeserializeObject<T5>(JsonConvert.SerializeObject(input.ElementAt(4)));
+                return new List<object>() { func(typedInput1, typedInput2, typedInput3, typedInput4, typedInput5) };
+            };
+        }
+    }
 }
