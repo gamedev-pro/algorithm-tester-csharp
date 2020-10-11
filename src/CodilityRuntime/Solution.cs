3938edd8c526d74e4daf8768eed9a34aae0a4e5e
Gabriel <gabrieldechichi@gmail.com>
2020-10-11 00:13:34 -0600

    Max Counter

--- src/CodilityRuntime/Solution.cs
+++ src/CodilityRuntime/Solution.cs
@@ -1,5 +1,4 @@
-﻿using CodilityRuntime.Extensions;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,31 +7,30 @@ class Solution
 {
     public int[] solution(int N, int[] A)
     {
-        //break A into arrays with the Max Operation in the end, and no MaxOperations in the beginning
-        //For each broken array, count the number of repeated values. This will be how much each counter value will increase at the end
-        //Set every counter to this max value, then iterate over the rest of operations doing the increases
+        var counters = new int[N];
+        var currentMaxCounter = 0;
+        var maxCounter = 0;
 
-        var splitIndexes = new List<int>(A.Length);
-        for (int i = 0; i < A.Length; i++)
+        foreach (var operation in A)
         {
-            //TODO: case where several max operations in a roll
-            if (IsMaxOperation(N, A[i]))
+            if (IsMaxOperation(N, operation))
             {
-                splitIndexes.Add(i);
+                maxCounter = currentMaxCounter;
+            }
+            else
+            {
+                var x = operation - 1;
+                counters[x] = CalculateCounterValueAtIndex(counters, x, maxCounter) + 1;
+                currentMaxCounter = counters[x] > currentMaxCounter ? counters[x] : currentMaxCounter;
             }
         }
 
-        int startCounter = 0;
-        foreach (var operations in SplitOperations(splitIndexes, A))
+        for (int i = 0; i < N; i++)
         {
-            if (IsMaxOperation(N, operations.Last()))
-            {
-
-            }
-            Console.WriteLine(operations.ToOutputString());
+            counters[i] = CalculateCounterValueAtIndex(counters, i, maxCounter);
         }
 
-        return new int[0];
+        return counters;
     }
 
     private bool IsMaxOperation(int countersNum, int operation)
@@ -40,15 +38,8 @@ class Solution
         return operation == countersNum + 1;
     }
 
-    private IEnumerable<IEnumerable<int>> SplitOperations(IEnumerable<int> splitIndexes, IEnumerable<int> operations)
+    private int CalculateCounterValueAtIndex(int[] counters, int counterIndex, int maxCounter)
     {
-        var remaining = operations;
-        foreach (var splitIndex in splitIndexes)
-        {
-            yield return remaining.Take(splitIndex + 1);
-            remaining = remaining.Skip(splitIndex + 1);
-        }
-
-        yield return remaining;
+        return counters[counterIndex] < maxCounter ? maxCounter : counters[counterIndex];
     }
 }
