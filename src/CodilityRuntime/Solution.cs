e7673840d06ccff1a969a1320c6bab9394e1c407
Gabriel <gabrieldechichi@gmail.com>
2020-10-11 17:41:33 -0600

    Min Avg

--- src/CodilityRuntime/Solution.cs
+++ src/CodilityRuntime/Solution.cs
@@ -5,66 +5,51 @@ using System.Text;
 
 class Solution
 {
-    public int[] solution(string S, int[] P, int[] Q)
+    public int solution(int[] A)
     {
-        //The solution is to create a separate array for each nucleotide, 
-        //with 0s and 1s (1 for when the nucleotide is in the given index) and the same length as S
-        //Then if the PrefixSum for a given range {P[k],Q[k]} will be > 0 if the given nucleotide is present in the range
-        //So, to find the minimum impact factor, we just need to go through each array of PrefixSum in order,
-        //and return whenever the sum is > 0
-        var separatedImpactFators = new Dictionary<char, int[]>
-        {
-            { 'A', new int[S.Length] },
-            { 'C', new int[S.Length] },
-            { 'G', new int[S.Length] },
-            { 'T', new int[S.Length] }
-        };
+        /*
+         * The key is knowing that there must be either a 2 or 3 sized slice with the minimum average (MA).
+         * This can be proved by the following:
+            * Consider a slice S of N elements, with N > 3, which is KNOWN that it contains the global minimum avg (MA)
+            * 1. If N is even, we can divide S in two sub-slices of length 2. Those slices must have the same average because:
+                * If they have different averages, one of them would HAVE to be lower than MA, which is a contradiction
+                * Thus, both sub-slices have the same avg which is equal MA
+            * 2. If N is odd we can divide S in two sub-slices of length 2 and 3. And the same conclusion as above follows,
+            * they MUST have the same average
+            * 
+            * Another explanation:
+                * The key point to understand in this proof is that NO sub-slice of a slice with the global MA can have an average
+                * smaller than MA (because it's a contradiction), nor greater than MA, because this would have to be compensated by
+                * another average smaller than MA, which leads to the same contradiction
+         */
 
-        for (int i = 0; i < S.Length; i++)
-        {
-            var nucleotide = S[i];
-            separatedImpactFators[nucleotide][i] = 1;
-        }
+        /*
+         * So the solution is, for every starting index (0 to A.Length - 1), test a 2 sized and 3 sized slice
+         */
 
-        var prefixSums = new Dictionary<char, PrefixSum>
-        {
-            { 'A', new PrefixSum(separatedImpactFators['A']) },
-            { 'C', new PrefixSum(separatedImpactFators['C']) },
-            { 'G', new PrefixSum(separatedImpactFators['G']) },
-            { 'T', new PrefixSum(separatedImpactFators['T']) },
-        };
+        var prefixSum = new PrefixSum(A);
+        var minAvg = double.MaxValue;
+        var bestStart = 0;
 
-        var queryAnswers = new List<int>(P.Length);
-        for (int i = 0; i < P.Length; i++)
+        for (int i = 0; i < A.Length - 1; i++)
         {
-            var start = P[i];
-            var end = Q[i];
-
-            foreach (var nucleotideToPrefixSum in prefixSums)
+            var size2Avg = prefixSum.GetAvg(i, Math.Min(i + 1, A.Length - 1));
+            if (size2Avg < minAvg)
             {
-                if (nucleotideToPrefixSum.Value.GetSum(start, end) > 0)
-                {
-                    queryAnswers.Add(GetImpactFactor(nucleotideToPrefixSum.Key));
-                    break;
-                }
+                minAvg = size2Avg;
+                bestStart = i;
+            }
+
+            var size3Avg = prefixSum.GetAvg(i, Math.Min(i + 2, A.Length - 1));
+            if (size3Avg < minAvg)
+            {
+                minAvg = size3Avg;
+                bestStart = i;
             }
         }
 
-        return queryAnswers.ToArray();
+        return bestStart;
     }
-
-    private int GetImpactFactor(char nucleotide)
-    {
-        return impactFactors[char.ToLower(nucleotide)];
-    }
-
-    private Dictionary<char, int> impactFactors = new Dictionary<char, int>
-    {
-        {'a', 1},
-        {'c', 2},
-        {'g', 3},
-        {'t', 4}
-    };
 
     public class PrefixSum
     {
@@ -80,6 +65,11 @@ class Solution
                 throw new System.Exception("Out of Range prefix sum exception");
             }
             return prefixSums[end + 1] - prefixSums[start];
+        }
+
+        public double GetAvg(int start, int end)
+        {
+            return GetSum(start, end) / (double) (end - start + 1);
         }
 
         private void BuildPrefixSums(IEnumerable<int> collection)
---
+++ src/CodilityRuntime/Solutions/5_PrefixSums/13_MinAvg.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodilityRuntime.Solutions._5_PrefixSums
+{
+    class MinAvg
+    {
+        public int solution(int[] A)
+        {
+            /*
+             * The key is knowing that there must be either a 2 or 3 sized slice with the minimum average (MA).
+             * This can be proved by the following:
+                * Consider a slice S of N elements, with N > 3, which is KNOWN that it contains the global minimum avg (MA)
+                * 1. If N is even, we can divide S in two sub-slices of length 2. Those slices must have the same average because:
+                    * If they have different averages, one of them would HAVE to be lower than MA, which is a contradiction
+                    * Thus, both sub-slices have the same avg which is equal MA
+                * 2. If N is odd we can divide S in two sub-slices of length 2 and 3. And the same conclusion as above follows,
+                * they MUST have the same average
+                * 
+                * Another explanation:
+                    * The key point to understand in this proof is that NO sub-slice of a slice with the global MA can have an average
+                    * smaller than MA (because it's a contradiction), nor greater than MA, because this would have to be compensated by
+                    * another average smaller than MA, which leads to the same contradiction
+             */
+
+            /*
+             * So the solution is, for every starting index (0 to A.Length - 1), test a 2 sized and 3 sized slice
+             */
+
+            var prefixSum = new PrefixSum(A);
+            var minAvg = double.MaxValue;
+            var bestStart = 0;
+
+            for (int i = 0; i < A.Length - 1; i++)
+            {
+                var size2Avg = prefixSum.GetAvg(i, Math.Min(i + 1, A.Length - 1));
+                if (size2Avg < minAvg)
+                {
+                    minAvg = size2Avg;
+                    bestStart = i;
+                }
+
+                var size3Avg = prefixSum.GetAvg(i, Math.Min(i + 2, A.Length - 1));
+                if (size3Avg < minAvg)
+                {
+                    minAvg = size3Avg;
+                    bestStart = i;
+                }
+            }
+
+            return bestStart;
+        }
+
+        public class PrefixSum
+        {
+            public PrefixSum(IEnumerable<int> collection)
+            {
+                BuildPrefixSums(collection);
+            }
+
+            public int GetSum(int start, int end)
+            {
+                if (start < 0 || start > end || end + 1 >= prefixSums.Length)
+                {
+                    throw new System.Exception("Out of Range prefix sum exception");
+                }
+                return prefixSums[end + 1] - prefixSums[start];
+            }
+
+            public double GetAvg(int start, int end)
+            {
+                return GetSum(start, end) / (double)(end - start + 1);
+            }
+
+            private void BuildPrefixSums(IEnumerable<int> collection)
+            {
+                prefixSums = new int[collection.Count() + 1];
+                for (int i = 1; i < prefixSums.Length; i++)
+                {
+                    prefixSums[i] = prefixSums[i - 1] + collection.ElementAt(i - 1);
+                }
+            }
+
+            private int[] prefixSums;
+        }
+    }
+}
