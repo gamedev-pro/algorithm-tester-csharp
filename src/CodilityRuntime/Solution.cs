cc1c661db2d8f33e5a304fc62081c9b9d850147b
Gabriel <gabrieldechichi@gmail.com>
2020-10-18 14:56:12 -0600

    Basic Map impl

--- src/CodilityRuntime/Solution.cs
+++ src/CodilityRuntime/Solution.cs
@@ -7,8 +7,18 @@ class Solution
 {
     public int solution(int[] A)
     {
-        var valueSet = new CodilityRuntime.Solutions.Extras.CoreDataStructs.HashSetWithDoubleList<int>(A);
-        Console.WriteLine(valueSet.ToString());
-        return valueSet.Count();
+        var dict = new CodilityRuntime.Solutions.Extras.CoreDataStructs.MapWithDoubleList<int, int>();
+        dict.Add(1, 1);
+        dict.Add(2, 1);
+        dict.Add(3, 3);
+        dict.Add(1, 67);
+        dict.Add(4, 1);
+        dict.Add(5, 3);
+        dict.Add(6, 3);
+
+        Console.WriteLine(dict.ToString());
+        Console.WriteLine(dict.Get(1));
+        Console.WriteLine(dict.Get(10));
+        return -1;
     }
 }
