1686f2fb33348f8c19a62b82cb761d06a6be7ba4
Gabriel <gabrieldechichi@gmail.com>
2020-11-13 22:21:08 -0700

    Distinct

--- src/CodilityRuntime/Solution.cs
+++ src/CodilityRuntime/Solution.cs
@@ -7,18 +7,7 @@ class Solution
 {
     public int solution(int[] A)
     {
-        var dict = new CodilityRuntime.Solutions.Extras.CoreDataStructs.MapWithDoubleList<int, int>();
-        dict.Add(1, 1);
-        dict.Add(2, 1);
-        dict.Add(3, 3);
-        dict.Add(1, 67);
-        dict.Add(4, 1);
-        dict.Add(5, 3);
-        dict.Add(6, 3);
-
-        Console.WriteLine(dict.ToString());
-        Console.WriteLine(dict.Get(1));
-        Console.WriteLine(dict.Get(10));
-        return -1;
+        var set = new HashSet<int>(A);
+        return set.Count();
     }
 }
---
+++ src/CodilityRuntime/Solutions/6_Sorting/15_Distinct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodilityRuntime.Solutions.Sorting
+{
+    class Distinct
+    {
+        public int solution(int[] A)
+        {
+            var set = new HashSet<int>(A);
+            return set.Count();
+        }
+    }
+}
