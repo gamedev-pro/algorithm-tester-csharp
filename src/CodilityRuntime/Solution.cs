f43758f564f6139c66224ad1b9dc17edf8c9ccce
Gabriel <gabrieldechichi@gmail.com>
2020-11-15 16:18:08 -0700

    DiscIntersectionCount

--- src/CodilityRuntime/Solution.cs
+++ src/CodilityRuntime/Solution.cs
@@ -1,79 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 
 class Solution
 {
+    class Circle
+    {
+        public long Center;
+        public long Radius;
+        public long Left { get { return Center - Radius; } }
+        public long Right { get { return Center + Radius; } }
+    }
+
+    /*
+     * Comparer that will choose the left most circle (either by left edge or center)
+     */
+    class LeftMostCircleOrderer : IComparer<Circle>
+    {
+        public int Compare([AllowNull] Circle x, [AllowNull] Circle y)
+        {
+            if (x.Left == y.Left)
+            {
+                return x.Center.CompareTo(y.Center);
+            }
+
+            return x.Left.CompareTo(y.Left);
+        }
+    }
     public int solution(int[] A)
     {
-        var threeMax = FindThreeMaxByComparison(A, (a, b) => a > b);
-        var threeMins = FindThreeMaxByComparison(A, (a, b) => a < b);
+        /*
+         * 1 - We order the circles by left most edge (or left most position)
+         * 2 - We go over the list of circles, storing the visited circles.
+         *     2.1 - For every circle, the non-repeat intersection count will be equal to the number of visited circles UNLESS
+         *     2.2 - Unless the visited circle's Right is less than the current circle's left, which means the visited circle won't ever
+         *     count as an intersection again, and we can remove it from the list
+         */
+        var circles = A
+            .Select((e, index) => new Circle { Center = index, Radius = e })
+            .ToArray();
 
-        var maxProduct = A[threeMax[0]] * A[threeMax[1]] * A[threeMax[2]];
-        var candidateMaxProduct = A[threeMax[2]] * A[threeMins[1]] * A[threeMins[2]];
-        maxProduct = candidateMaxProduct > maxProduct ? candidateMaxProduct : maxProduct;
+        Array.Sort(circles, new LeftMostCircleOrderer());
 
-        return maxProduct;
-    }
-
-    private int[] FindThreeMaxByComparison(int[] A, Func<int, int, bool> comparisonFunc)
-    {
-        var firstThrees = new int[] { A[0], A[1], A[2] };
-        var max = FindMaxIndexByComparison(firstThrees, comparisonFunc);
-        var secondMax = -1;
-        var thirdMax = -1;
-        if (max == 0)
+        var intersectingCircles = new List<Circle>(circles.Length);
+        var intersections = 0;
+        for (int i = 0; i < circles.Length; i++)
         {
-            secondMax = comparisonFunc(A[1],A[2]) ? 1 : 2;
-            thirdMax = comparisonFunc(A[1], A[2]) ? 2 : 1;
-        }
-        else if (max == 1)
-        {
-            secondMax = comparisonFunc(A[0],A[2]) ? 0 : 2;
-            thirdMax = comparisonFunc(A[0], A[2]) ? 2 : 0;
-        }
-        else if (max == 2)
-        {
-            secondMax = comparisonFunc(A[0],A[1]) ? 0 : 1;
-            thirdMax = comparisonFunc(A[0], A[1]) ? 1 : 0;
-        }
-
-        for (int i = 3; i < A.Length; i++)
-        {
-            if (comparisonFunc(A[i],A[max]))
+            //Go over intersecting circles, and removing the ones that shouldn't be count as an intersection
+            //(any in which Right is less than the current circle's Left)
+            for (int j = intersectingCircles.Count - 1; j >= 0; j--)
             {
-                var temp = max;
-                var temp2 = secondMax;
-                max = i;
-                secondMax = temp;
-                thirdMax = temp2;
+                if (intersectingCircles[j].Right < circles[i].Left)
+                {
+                    intersectingCircles.RemoveAt(j);
+                }
             }
-            else if (comparisonFunc(A[i],A[secondMax]))
-            {
-                var temp = secondMax;
-                secondMax = i;
-                thirdMax = temp;
-            }
-            else if (comparisonFunc(A[i], A[thirdMax]))
-            {
-                thirdMax = i;
-            }
-        }
-        return new int[] { thirdMax, secondMax, max };
-    }
 
-    private int FindMaxIndexByComparison(int[] a, Func<int, int, bool> comparisonFunc)
-    {
-        var maxIndex = 0;
-        for (int i = 1; i < a.Length; i++)
-        {
-            if (comparisonFunc(a[i], a[maxIndex]))
+            //The non-repeat intersection count will equal que number of elements in the intersection list
+            intersections += intersectingCircles.Count;
+            if (intersections > 1e7)
             {
-                maxIndex = i;
+                return -1;
             }
+
+            //Add ourselves to the possible intersectingCircles for the next run
+            intersectingCircles.Add(circles[i]);
         }
 
-        return maxIndex;
+        return intersections;
     }
 }
