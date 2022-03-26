894523ce515d376fc8df3b186e23a63c55438e09
Gabriel Dechichi <gabrieldechichi@gmail.com>
2022-03-26 10:30:16 -0700

    Output colored text

--- src/AlgTester/Program.cs
+++ src/AlgTester/Program.cs
@@ -1,8 +1,8 @@
-﻿using CodilityRuntime.Core;
+﻿using System;
+using System.Collections.Generic;
+using CodilityRuntime.Core;
 using CodilityRuntime.Extensions;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
 
 namespace CodilityRuntime
 {
@@ -20,11 +20,13 @@ namespace CodilityRuntime
                 throw new System.Exception("Codility solution function is null");
             }
 
+            var comparer = new CodilityOutputComparer<IEnumerable<object>>();
             int testIndex = 0;
             foreach (var testCase in testSuite)
             {
                 var actual = func(testCase.Input);
-
+                var passed = comparer.Equals(actual, testCase.Output);
+                Console.ForegroundColor = passed ? ConsoleColor.Green : ConsoleColor.Red;
                 Console.WriteLine(
                     string.Format("Test {0}: Input = {1}, Expected = {2}, Actual = {3}", 
                     testIndex,
